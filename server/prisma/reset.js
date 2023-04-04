75e2950b0a64b6530130987171be5f22a393f9f7
shy0980 <laughygamer@gmail.com>
2023-04-04 19:06:11 +0530

    hmm

--- server/prisma/reset.js
+++ server/prisma/reset.js
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function seed() {
+  await prisma.comment.deleteMany()
   await prisma.post.deleteMany()
   await prisma.user.deleteMany()
 }
